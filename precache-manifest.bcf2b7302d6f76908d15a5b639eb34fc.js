3d5be1a70789a061806a44e6b51b372579ac1a6d
cat <youssefannabi@gmail.com>
2020-07-14 11:44:59 +0100

    Updates

---
+++ precache-manifest.bcf2b7302d6f76908d15a5b639eb34fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0907e2f049c8728e4952523b7d810f65",
+    "url": "/checkpoint-jsx/index.html"
+  },
+  {
+    "revision": "b29783e750fe109feb8c",
+    "url": "/checkpoint-jsx/static/css/main.9da5e73c.chunk.css"
+  },
+  {
+    "revision": "ad6dce309e180cb67aa2",
+    "url": "/checkpoint-jsx/static/js/2.6a30f4bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkpoint-jsx/static/js/2.6a30f4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29783e750fe109feb8c",
+    "url": "/checkpoint-jsx/static/js/main.e1ff6587.chunk.js"
+  },
+  {
+    "revision": "6a734000998d183d4015",
+    "url": "/checkpoint-jsx/static/js/runtime-main.e79cd693.js"
+  },
+  {
+    "revision": "95797883a99bfc04f589d05bee377ac5",
+    "url": "/checkpoint-jsx/static/media/img.95797883.png"
+  }
+]);
